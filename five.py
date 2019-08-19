c736d1bafa433c478104e8b98428de8b64f569ae
Lynesn <48829332+Lynesn@users.noreply.github.com>
2019-08-19 15:17:57 +0300

    Update five.py
    Edited changes.

--- five.py
+++ five.py
@@ -1,9 +1,13 @@
 # Write a function that returns the largest number in a tuple (Using Arbitrary Arguments will earn you
 # bonus marks)
 
-def largest():                   #define a function largest
-    myT = max(5, 20, 40, 100)     #create a variable/ tupple myT
-    return(myT)                    # return and store the maximum value form tuple myT               
 
-largest()                                     # Call the function largest
-print("The largest number is:", largest())    # print The largest number is , largest
+def largest(*numbers):  # define a function and pass it all numbers
+    largeNum = 0  # variable largeNum
+    for item in numbers:              # loop through the numbers
+        if item > largeNum:        # if item is greater than, replace it with the larger num
+            largeNum = item
+
+    return largeNum
+
+print(largest(23, 45, 900, 7000, 54, 23, 21, 1))
